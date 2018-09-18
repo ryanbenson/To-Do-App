5da262905ee9c29b423082d68f9fa438a1102984
Ryan Benson <benson.ryan@gmail.com>
2018-09-18 16:23:13 -0500

    Cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Main from './Main';
 
 function mapStateToProps(state) {
   return {
-    tasks: state.tasks,
+    tasks: state.tasks
   };
 }
 
--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,23 +1,18 @@
 function posts(state = [], action) {
   switch(action.type) {
     case 'GET_TASKS':
-      console.log("GET ALL TASKS");
       return state;
     case 'GET_TASKS_SUCCESS':
       return state.tasks = action.tasks;
     case 'GET_TASKS_FAILURE':
-      console.log('FAILED TO GET TASKS');
       return state;
     case 'GET_TASK':
-      console.log("GET SINGLE TASK");
       return state;
     case 'ADD_TASK':
-      console.log("ADDING A TASK");
       return state;
     case 'ADD_TASK_SUCESS':
       return [...state, {name: action.data.name, id: action.data.id}];
     case 'EDIT_TASK':
-      console.log("EDITING A TASK");
       return state;
     case 'REMOVE_TASK':
       return state;
